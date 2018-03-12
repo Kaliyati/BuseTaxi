6bb67301cbb0efca2d91e0c55718903b5c51d7a0
Int-Systems <kaliyatilee@gmail.com>
2018-03-12 17:44:56 +0200

    maps activity

--- app/src/main/java/com/example/int_systems/busetaxi/RaiderMainActivity.java
+++ app/src/main/java/com/example/int_systems/busetaxi/RaiderMainActivity.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.int_systems.busetaxi.*;
@@ -17,34 +19,35 @@ import com.example.int_systems.busetaxi.*;
 public class RaiderMainActivity extends AppCompatActivity {
     static final int REQUEST_LOCATION = 1;
     LocationManager locationManager;
+    boolean GpsStatus ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_raider_main);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        GPSStatus();
 
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-
-        } else {
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-            if (location != null) {
-                double latti = location.getLatitude();
-                double longi = location.getLongitude();
-
-                Toast.makeText(RaiderMainActivity.this,"hie", Toast.LENGTH_LONG).show();
-
-                System.out.println("+++++++++++++++++++++++++");
-                System.out.println(latti);
-  }
-
+        if(GpsStatus == true)
+        {
+            System.out.println("GPS ENABLED");
+        }else
+        {
+          System.out.println("GPS NOT ENABLED");
         }
+       Intent intent1 = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent1);
     }
+
+    public void GPSStatus(){
+        locationManager = (LocationManager)RaiderMainActivity.getSystemService(Context.LOCATION_SERVICE);
+        GpsStatus = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+
+
+
 }
+
+
+
